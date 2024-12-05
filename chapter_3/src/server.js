a1c9d16c04304645496018bb0c6d3ad62945e579
Kiel <ezetalampas@gmail.com>
2024-12-05 15:21:33 +0800

    update

--- chapter_3/src/server.js
+++ chapter_3/src/server.js
@@ -3,36 +3,31 @@ import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import authRoutes from './routes/authRoutes.js'
 import todoRoutes from './routes/todoRoutes.js'
-
-
+import authMiddleware from './middleware/authMiddleware.js'
 
 const app = express()
 const PORT = process.env.PORT || 5003
 
-//get file path from URL of the current module
+// Get the file path from the URL of the current module
 const __filename = fileURLToPath(import.meta.url)
-
-// get dir name from file path
+// Get the directory name from the file path
 const __dirname = dirname(__filename)
 
-//middleware
+// Middleware
 app.use(express.json())
-
-//serves html file from public dir
+// Serves the HTML file from the /public directory
+// Tells express to serve all files from the public folder as static assets / file. Any requests for the css files will be resolved to the public directory.
 app.use(express.static(path.join(__dirname, '../public')))
 
-
-//Serving up html file from dir
+// Serving up the HTML file from the /public directory
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 
-//routes
+// Routes
 app.use('/auth', authRoutes)
-app.use('/todos', todoRoutes)
-
-
+app.use('/todos', authMiddleware, todoRoutes)
 
 app.listen(PORT, () => {
-    console.log(`server started: ${PORT}`)
+    console.log(`Server has started on port: ${PORT}`)
 })
